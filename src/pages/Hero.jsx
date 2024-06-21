70f3d5588ccdee60b4147a7207792696f754e520
Chris Mungiria <christopher.mungiria@strathmore.edu>
2024-06-21 17:12:59 +0300

    feat: added CTA buttons

--- src/pages/Hero.jsx
+++ src/pages/Hero.jsx
@@ -11,6 +11,22 @@ const Hero = () => {
           Haki Hub: Empowering Kenyans with updates on ongoing bills, protest
           information, and essential data for informed civic engagement.
         </p>
+        <div className="mt-5 flex items-center gap-5">
+          <a
+            href="https://docs.google.com/forms/d/e/1FAIpQLSenaZEFvO7OXLOxroNZKW8UTCPZVGgNgLHHAKOjg6sE5FuUbw/viewform"
+            target="blank"
+            className="bg-[#B9121B] text-[#FCE7E3] px-4 py-2 rounded-md hover:bg-[#ECA192] hover:text-[#651813] hover:shadow-lg transition-all duration-200 ease-in-out"
+          >
+            Give feedback
+          </a>
+          <a
+            href="https://docs.google.com/forms/d/e/1FAIpQLSfIXa9Kbk9HmxP-GWBwF3a__D19OUitxkOSuUv-6cgwNjoaNA/viewform"
+            target="blank"
+            className="bg-[#2E7D32] text-[#E8F0E7] px-4 py-2 rounded-md hover:bg-[#A4C4A0] hover:text-[#204520]  hover:shadow-lg transition-all duration-200 ease-in-out"
+          >
+            Help build Haki Hub
+          </a>
+        </div>
       </div>
       <div className="flex-1">
         {/* Import an svg called Kenya.svg */}
