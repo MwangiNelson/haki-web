3d55d90892c97264612416699252739cd9dab28c
Chris Mungiria <christopher.mungiria@strathmore.edu>
2024-06-26 22:42:02 +0300

    fix: made the navbar responsive

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -21,29 +21,31 @@ const sections = [
 
 const Navbar = ({ currentSection }) => {
   return (
-    <nav className="w-full p-5 px-16 bg-[#f0f0f081] backdrop-blur-3xl border-b flex flex-col md:flex-row items-center justify-between gap-5 fixed top-0 z-10">
+    <nav className="w-full p-5 md:px-16 bg-[#f0f0f081] backdrop-blur-3xl border-b flex flex-col md:flex-row items-center justify-between gap-5 fixed top-0 z-10">
       <p className="text-xl font-semibold text-[#D05848]">
         #RejectFinanceBill2024
       </p>
-      {currentSection &&
-        sections.map((section) => {
-          return (
-            <ul key={section.id}>
-              <a
-                href={`#${section.id}`}
-                className={`
+      <ul className="flex flex-row gap-5 md:gap-10">
+        {currentSection &&
+          sections.map((section) => {
+            return (
+              <li key={section.id}>
+                <a
+                  href={`#${section.id}`}
+                  className={` text-sm md:text-base
                 ${
                   section.id === currentSection
                     ? "text-slate-800"
                     : "text-slate-400"
                 }
                 `}
-              >
-                {section.title}
-              </a>
-            </ul>
-          );
-        })}
+                >
+                  {section.title}
+                </a>
+              </li>
+            );
+          })}
+      </ul>
       <a
         href="https://github.com/DanroyMwangi/haki-hub/raw/develop/apks/HakiHub.apk"
         className="flex items-center text-xs md:text-base gap-4 bg-[#2E7D32] text-slate-100 px-6 py-3 rounded-md shadow-xl hover:shadow-md transition-all duration-150 ease-in-out"
